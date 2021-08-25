ae39d561dc950b15597751f5fe15a5e6d757c17e
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2021-08-25 17:55:21 +0900

    add Datasets

--- timit_data_processor.py
+++ timit_data_processor.py
@@ -1,10 +1,13 @@
 import argparse
+import os
 
 import numpy as np
 import pickle
 import pandas as pd
 import soundfile as sf
 from scipy import signal
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
 
 
 phn_dict = {}
@@ -66,6 +69,68 @@ def feature_extraction(path,csvname,fwfdst):
     df.to_csv(fwfdst)
     return x,y
 
+class Timit(Dataset):
+    def __init__(self, annotations_file, data_dir, n_fft=512):
+        df = pd.read_csv(annotations_file)
+        df = df.sort_values('path_from_data_dir')
+    
+        self.df_wav = df[df['filename'].str.endswith('.WAV',na=False)]
+        self.df_phn = df[df['filename'].str.endswith('.PHN',na=False)]
+
+        self.data_dir = data_dir
+        self.n_fft = n_fft
+
+    def __len__(self):
+        return len(self.df_wav)
+
+    def __getitem__(self, idx):
+        wav_path = os.path.join(self.data_dir, self.df_wav.iat[idx, 5])
+        sign, sr = sf.read(wav_path)
+        spec = signal.stft(sign,sr,nperseg=self.n_fft)[2]
+
+        phn_path = os.path.join(self.data_dir, self.df_phn.iat[idx, 5])
+        df = pd.read_csv(phn_path, delimiter=' ', header=None)
+        label = np.zeros(spec.shape[1])
+        noverlap = self.n_fft//2
+
+        global phn_dict
+        global phn_list
+        global phn_count
+
+        for i in range(len(df)):
+            begin = df.iat[i,0]//noverlap
+            end = df.iat[i,1]//noverlap
+            phn = df.iat[i,2]
+            if not phn in phn_dict:
+                phn_dict[phn] = phn_count
+                phn_list.append(phn)
+                phn_count += 1
+            label[begin:end] = phn_dict[phn]
+
+        return spec,label
+
+class FramedTimit(Dataset):
+    def __init__(self, annotations_file, npz_dir, transform=None, target_transform=None):
+        self.annotations = pd.read_csv(annotations_file)
+        self.npz_dir = npz_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return self.annotations['max'].max()
+
+    def __getitem__(self, idx):
+        df = self.annotations[(self.annotations['min']<=idx)&(self.annotations['max']>idx)]
+        npz_path = os.path.join(self.npz_dir, df.iat[0, 0])
+        local_idx = idx - df.iat[0, 1]
+        npz = np.load(npz_path)
+        x = npz['x'][local_idx]
+        y = npz['y'][local_idx]
+        if self.transform:
+            x = self.transform(x)
+        if self.target_transform:
+            y = self.target_transform(y)
+        return x,y
 
 def main():
     parser = argparse.ArgumentParser(description="process timit data")
@@ -81,8 +146,45 @@ def main():
     f = open('phn.pickle', 'wb')
     pickle.dump(phn_list, f)
     
-    
+def load_timit():
+    parser = argparse.ArgumentParser(description="load TIMIT and convert to npz file")
+    parser.add_argument("path", type=str, help="path to the directory that has annotation files")
+    args = parser.parse_args()
+
+    train_data = Timit(os.path.join(args.path, 'train_data.csv'),
+                       os.path.join(args.path, 'data/'))   
+    test_data = Timit(os.path.join(args.path, 'test_data.csv'),
+                      os.path.join(args.path, 'data/'))     
+
+    train_dataloader = DataLoader(train_data, batch_size=1)
+    test_dataloader = DataLoader(test_data, batch_size=1)
+
+    count = 0
+    annotation = []
+    max = 0
+    for spec,label in train_dataloader:
+        path = os.path.join(args.path, "data/npz/TRAIN/", f"data{count}")
+        np.savez(path, spec=spec, label=label)
+        annotation.append({'path':f"{path}.npz",'min':max,'max':max+label.size})
+        count += 1
+        max += label.size
+
+    df = pd.DataFrame(annotation)
+    df.to_csv(os.path.join(args.path, 'train_npz.csv'))
+
+    count = 0
+    annotation = []
+    max = 0
+    for spec,label in test_dataloader:
+        path = os.path.join(args.path, "data/npz/TEST/", f"data{count}")
+        np.savez(path, spec=spec, label=label)
+        annotation.append({'path':f"{path}.npz",'min':max,'max':max+label.size})
+        count += 1
+        max += label.size
+
+    df = pd.DataFrame(annotation)
+    df.to_csv(os.path.join(args.path, 'test_npz.csv'))
 
 
 if __name__=="__main__":
-    main()
+    load_timit()
