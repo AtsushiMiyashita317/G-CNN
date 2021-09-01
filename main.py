232c67e6c388e738c28b1bedbec6ab9938286ec6
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2021-09-01 09:54:31 +0900

    main

--- main.py
+++ main.py
@@ -35,6 +35,37 @@ class Model(nn.Module):
         logits = self.layer_stack(x)
         return logits
 
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device, dtype=torch.float), y.to(device)
+        
+        # 損失誤差を計算
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        
+        # バックプロパゲーション
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 1 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= size
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 def main():
     parser = argparse.ArgumentParser(description="test class FramedTimit")
@@ -67,23 +98,12 @@ def main():
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    size = len(train_dataloader.dataset)
-    for epoc in range(10):
-        for batch, (X, y) in enumerate(train_dataloader):
-            X, y = X.to(device=device, dtype=torch.float), y.to(device=device, dtype=torch.long)
-            
-            # 損失誤差を計算
-            pred = model(X)
-            loss = loss_fn(pred, y)
-            
-            # バックプロパゲーション
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            if batch % 1 == 0:
-                loss, current = loss.item(), batch * len(X)
-                print(f"epoc: {epoc:>2d} loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    epochs = 5
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    print("Done!")
 
 
 if __name__=="__main__":
