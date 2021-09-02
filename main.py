8126058bd6b7b88f36304621c122efff4d577adf
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2021-09-02 13:09:10 +0900

    modify some

--- main.py
+++ main.py
@@ -36,39 +36,6 @@ class Model(nn.Module):
         logits = self.layer_stack(x)
         return logits
 
-def train(dataloader, model, loss_fn, optimizer, device):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device, dtype=torch.float), y.to(device, dtype=torch.long)
-        
-        # 損失誤差を計算
-        pred = model(X)
-        loss = loss_fn(pred, y)
-        acc = (pred.argmax(1) == y).type(torch.float).sum().item()/y.shape[0]
-        
-        # バックプロパゲーション
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if batch % 1 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f} acc: {acc:>3f} [{current:>5d}/{size:>5d}]")
-
-def test(dataloader, model, loss_fn, device):
-    size = len(dataloader.dataset)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device, dtype=torch.float), y.to(device, dtype=torch.long)
-
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= size
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 def main():
     parser = argparse.ArgumentParser(description="test class FramedTimit")
