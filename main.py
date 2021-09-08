597a88793488bb215a4623763c30a74140a315c1
AtsushiMiyashita317 <miyashita.atsushi@g.sp.m.is.nagoya-u.ac.jp>
2021-09-08 13:19:46 +0900

    learning

--- main.py
+++ main.py
@@ -31,55 +31,11 @@ class Model(nn.Module):
         logits = self.layer_stack(x)
         return logits
 
-def train(dataloader, model, loss_fn, optimizer, device):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device, dtype=torch.float), y.to(device, dtype=torch.long)
-        
-        # 損失誤差を計算
-        pred = model(X)
-        loss = loss_fn(pred, y)
-        acc = (pred.argmax(1) == y).type(torch.float).sum().item()/y.shape[0]
-        
-        # バックプロパゲーション
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f} acc: {acc:>3f} [{current:>5d}/{size:>5d}]")
-
-
-def test(dataloader, model, loss_fn, device):
-    size = len(dataloader.dataset)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device, dtype=torch.float), y.to(device, dtype=torch.long)
-
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= size
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-
 def main():
     parser = argparse.ArgumentParser(description="test class FramedTimit")
     parser.add_argument("path", type=str, help="path to the directory that has annotation files")
     parser.add_argument("exname", type=str, help="the name of experiment")
     args = parser.parse_args()
-
-    n_fft = 256
-    vtl = transform.VTL(n_fft,np.tanh(np.linspace(-0.5,0.5,9)),dropphase=True)
-    mel = transform.MelScale(n_fft,n_mels=40)
-    trans = transform.Function(np.transpose)
-
-
-    composed1 = transforms.Compose([vtl,mel])
 
     train_data = MyDataset(args.path, 'TRAIN')
     test_data = MyDataset(args.path, 'TEST')
